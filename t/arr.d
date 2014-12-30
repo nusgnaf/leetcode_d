d74a3edbf87a7b79286746fcddc84b31f0b781a2
fang <nusgnaf@foxmail.com>
2014-12-31 06:28:15 +0800

    Code repo for leetcode snippets

---
+++ t/arr.d
@@ -0,0 +1,14 @@
+﻿module t.arr;
+
+import std.array;
+
+unittest {
+	auto a = array([1,2,3,4,5][]);
+	assert(a == [1,2,3,4,5]);
+	//std.stdio.writeln(a);
+}
+
+bool leapYear(uint digi)
+{
+	return true;
+}
