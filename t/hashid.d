ff980034917d63af7c940a8fff9f13a5d80952f2
fang <nusgnaf@foxmail.com>
2015-01-04 15:29:09 +0800

    add hashid module, to be implemented.

---
+++ t/hashid.d
@@ -0,0 +1,27 @@
+﻿module t.hashid;
+
+
+import std.stdio;
+
+unittest {
+	string bs10 = "0123456789";
+	string bs16 = "0123456789abcdef";
+	string bs62 = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	assert(encode(7,bs10) == "7");
+	assert(encode(7,bs16) == "7");
+	assert(encode(12, bs16) == "c");
+	assert(encode(255, bs16) == "ff");
+}
+
+
+string encode(uint input, string alps)
+{
+	string hash = "";
+	uint l = cast(uint) alps.length;
+	do {
+		hash = alps[input % l] ~ hash;
+		input = input/l ;
+	} while(input);
+	return hash;
+}
+
