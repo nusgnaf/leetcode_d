bceaaf6f242690fecd8714a28850faf2e539b9bc
fang <nusgnaf@foxmail.com>
2015-01-05 17:57:33 +0800

    from cehreli's dconf 2013 talk.

---
+++ t/cpymv.d
@@ -0,0 +1,29 @@
+﻿module t.cpymv;
+
+struct S
+{
+	// payload!
+	int[] data;
+
+	this(string s) {
+		data.length = 42;
+	}
+
+	this(this) {
+		// postblit your data
+		data = data.dup;
+	}
+
+	this(S rhs) {
+	}
+
+	this(ref const(S) rhs) {
+	}
+
+	ref S opAssign(S rhs) {
+	}
+
+	ref S opAssign(ref const(S) rhs) {
+	}
+
+}
