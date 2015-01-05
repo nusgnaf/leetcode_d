08e3e6df5952c9eb82523cb5e61163cc0ff8bc55
fang <nusgnaf@foxmail.com>
2015-01-06 00:15:27 +0800

    upside down and down to upper
    * begin the constructor
    * finish the consistent shuffle function(!)
    * add str.d: try some string manipulation function

--- t/hashid.d
+++ t/hashid.d
@@ -1,7 +1,7 @@
 ﻿module t.hashid;
 
 
-import std.stdio;
+import std.stdio, std.exception;
 
 unittest {
 	string bs10 = "0123456789";
@@ -26,9 +26,52 @@ string encode(uint input, string alps)
 	return hash;
 }
 
-string FYshuffle(string alps, string salt)
-{
-	int i,j,v,p;
+unittest {
+}
 
-	return "";
+string FYshuffle(string alps, string salt)
+in {
+	assert(alps.length < 128);
+}
+
+body {
+	uint j,v,p;
+	char tmp, charCode;
+	char[] wkstr = alps.dup;
+	ulong i = cast(uint) alps.length-1;
+	for(; i>0; i--, p++) {
+		v %= salt.length;
+		p += charCode = salt[v];
+		j = (charCode+v+p)%i;
+
+		tmp = wkstr[j];
+		wkstr = wkstr[0..j] ~ wkstr[i] ~ wkstr[j+1..$];
+		wkstr = wkstr[0..i] ~ tmp ~ wkstr[i+1..$];
+	}
+	return assumeUnique(wkstr);
+}
+
+struct Hashids
+{
+	static string ALPHABET = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+	static int minAlphabetLength = 16;
+
+
+private:
+	string salt;
+	uint minHashLength;
+	string alphabet;
+
+	this(string salt, uint minHashLength, string alphabet)
+	{
+		enforce(alphabet.length > minAlphabetLength, "error: alphabet must contain at least 16 unique characters");
+		salt = salt;
+		minHashLength = minHashLength;
+		alphabet = alphabet;
+	}
+
+	this(string salt)
+	{
+		this(salt, 0, Hashids.ALPHABET);
+	}
 }
