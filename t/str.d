f4449c560cb89c8281ad610ffc1ee9a16a128fcf
fang <nusgnaf@foxmail.com>
2015-01-07 10:43:54 +0800

    more string unittest!

--- t/str.d
+++ t/str.d
@@ -21,23 +21,37 @@ unittest {
 	auto j = indexOfAny("helloWorld", "wr", CaseSensitive.no);
 	assert(k == 7);
 	assert(j == 5);
+	assert(capitalize("fAnG") == "Fang");
+	assert(center("fang", 10) == "   fang   ");
+	assert(center("fan", 10) ==  "   fan    ");
+	assert(chomp(" hello world  \n\r") == " hello world  \n");
+	assert(chomp(" hello world  \r\n") == " hello world  ");
+	assert(chomp(" hello world  \n\n") == " hello world  \n");
+	assert(chomp(" hello world", "orld") == " hello w");
+	assert(chomp(" hello world", " he") == " hello world");
+	assert(column("\t") == 8);
+	assert(column("\thaha") == 12);
+	assert(column("\t", 4) == 4);
+	assert(countchars("faint", "fuck") == 1);
+	assert(removechars("faint", "fuck") == "aint");
+	assert(detab("\tOK") == "        OK");
+	assert(detab("\tOK", 4) == "    OK");
+	assert(entab("    good") == "    good");
+	assert(entab("    good", 4) == "\tgood");
+	assert(entab("     good", 4) == "\t good");
+	assert(format("k=%s;v=%s", 1, 2) == "k=1;v=2");
+	assert(icmp("a","b") == -1);
+	assert(icmp("b","a") == 1);
+	assert(icmp("a","A") == 0); //  case incensitive!
 }
 
 
 void main()
 {
-	string u = "hahaha";
-	string v = u_change(u);
-	writeln(u, ":", v);
-	auto k = indexOfAny("helloWorld", "wr");
-	auto j = indexOfAny("helloWorld", "wr", CaseSensitive.no);
-	writeln(k,":",j);
-	static string[] list = [ "food", "foxy" ,"fuck","ford", "foo", "faint", "fang"];
-	
-	auto abbrevs = std.string.abbrev(list);
-	
-	foreach (key, value; abbrevs)
-	{
-		writefln("%s => %s", key, value);
-	}
+	writeln(countchars("faint", "fuck"));
+	writeln(removechars("faint", "fuck"));
+	writeln(format("k=%s;v=%s", 1, 2));
+	writeln(icmp("a","b"));
+	writeln(icmp("a","A"));
+	writeln(icmp("b","a"));
 }
