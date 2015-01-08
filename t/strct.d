3bf43d4a2471097ddcaa23dcc038742017fd37b5
fang <nusgnaf@foxmail.com>
2015-01-08 20:07:11 +0800

    some struct and array processing

--- t/strct.d
+++ t/strct.d
@@ -10,9 +10,26 @@ unittest {
 	S b = a;
 	a.data[1] = 1;
 	assert(b.data[1] == 1);
+	assert(S2!int.I == 1);
+	S2!int c = S2!int([1,2,3]);
+	assert(c.data == [2,4,6]);
+	auto d = c.data;
+	assert(d == [2,4,6]);
+	auto e = new int[3];
+	e[] = d[]/2;
+	assert(e == [1,2,3]);
+//	assert((d[])/2 == [1,2,3]);
 }
 
-struct S2
+
+//templized struct, super easy
+struct S2(T)
 {
-	int[] data;
+	static int I = 1;
+	T[] data;
+	this(T[] rawData)
+	{
+		data = new T[rawData.length];
+		data[] = 2*rawData[];
+	}
 }
