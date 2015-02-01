adae77f097a5f75b1524fe6c8feaa103f504b9d4
fang <nusgnaf@foxmail.com>
2015-02-01 23:05:52 +0800

    finding divisors of positive number is art!
    * add some wikipedia article
    * factor out more things

--- guiguzi/main.d
+++ guiguzi/main.d
@@ -1,26 +1,15 @@
-﻿module main;
+﻿// http://en.wikipedia.org/wiki/Divisor
+// http://en.wikipedia.org/wiki/Table_of_divisors
+
+module main;
 
 import std.range,std.array,std.algorithm;
 import std.stdio;
+import std.exception;
+import std.getopt;
+
 
 short[4] smallPrimes = [2,3,5,7];
-/***
- * factor out a small num
- */
-int[] factor(int num, in short[] smallPrimes)
-{
-	int[] res;
-//	short i;
-	foreach(i, p; smallPrimes) {
-		if((num % p) == 0 && (num != p)) {
-			res ~= p;
-			res ~= factor(num/p, smallPrimes[i..$]);
-			return res;
-		}
-	}
-	res ~= num; // num is prime!
-	return res;
-}
 
 /***
  * check some num is a prime or not
@@ -52,14 +41,33 @@ short[4] small = [2,3,5,7];
 
 struct Factor {
 	int[] facts;
+	int[int] primepower;
 	size_t count;
 	int num;
 	
 	this(int num)
 	{
 		this.num = num;
-		this.facts = factor(num, smallPrimes);
+		factor(num, smallPrimes, this.facts, this.primepower);
 		this.count = this.facts.length;
+	}
+	/***
+	 * factor out a small num
+	 */
+	void factor(int num, in short[] smallPrimes, ref int[] facts, ref int[int] primepower)
+	{
+		// int[] res;
+		//	short i;
+		foreach(i, p; smallPrimes) {
+			if((num % p) == 0 && (num != p)) {
+				facts ~= p; primepower[p] += 1;
+				factor(num/p, smallPrimes[i..$], facts, primepower);
+				return;
+			}
+		}
+		// num is prime!
+		facts ~= num; primepower[num] += 1;
+		return;
 	}
 
 	string toString()
@@ -69,13 +77,17 @@ struct Factor {
 	}
 }
 
+
 void main(string[] args)
 {
+	int n;
+	getopt(args, "num|n", &n);
 	Factor[int] result;
 	foreach(int i; 2 .. 99){
 		result[i] = Factor(i);
 	}
-	writeln(result);
+	writeln(result[n].primepower);
+	writeln(result[n].facts);
 	// select prime numbers that are less than 55
 	auto r1 = filter!("a < 55")( filter!isprime(iota(2, 100)) );
 	// select odd prime numbers that are less than 55
@@ -84,13 +96,17 @@ void main(string[] args)
 	auto r4 = iota(5, 55, 2);
 	// use map to add up 2 to r2
 	auto r3 = map!("a+2")(r2);
-	// r2 intersec r3
-	writeln(setIntersection(r4, r3));
-	// r2\r3
-	writeln(setDifference(r4,r3));
+	// r4 intersec r3
+//	writeln(setIntersection(r4, r3));
+	// r4\r3
+	auto a0 = array(setDifference(r4,r3));
+	writeln(a0);
+	auto r5 = assumeSorted(a0);
+	writeln(r5.contains(11));
+//	writeln(r5.contains(11));
 //	remove(r2, r3);
 //	foreach(e; r3) { writeln(e);}
-	writeln(r4);
+//	writeln(r4);
 //	writeln(filter!("a < 55")( filter!isprime(iota(2, 100)) ));
 //	writeln(filter!(q{a%2 == 0	})(iota(2, 100)));
 }
